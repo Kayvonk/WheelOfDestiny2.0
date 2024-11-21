c7b22e065c97cb454998083a771b880f21de361f
Kayvon Kazemini <kayvonk@gmail.com>
2024-11-21 01:30:48 -0700

    Updated name array logic

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>The Wheel of Destiny</h1>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/Wheel/index.jsx
+++ src/components/Wheel/index.jsx
@@ -2,76 +2,200 @@ import { useState } from "react";
 import "./style.css";
 
 const Wheel = () => {
-  const namesArray = [
+  let namesArray = [
     "Mario Mario",
     "Luigi Mario",
     "Link Hyrule",
     "Zelda Hyrule",
+    "Cloud Strife",
+    "Tifa Lockhart",
+    "Barret Wallace",
+    "Aerith Gainsborough",
+    "Squall Leonhart",
+    "Rinoa Heartilly",
+    "Zidane Tribal",
+    "Vivi Ornitier",
+    "Freya Crescent",
+    "Noctis Lucis Caelum",
+    "Ignis Scientia",
+    "Gladiolus Amicitia",
+    "Prompto Argentum",
+    "Cecil Harvey",
+    "Rosa Farrell",
+    "Lara Croft",
+    "Nathan Drake",
+    "Joel Miller",
+    "Ellie Williams",
+    "Kratos Spartan",
+    "Atreus Kratos",
+    "Geralt Rivia",
+    "Ciri Rivia",
+    "Triss Merigold",
+    "Dante Sparda",
+    "Vergil Sparda",
+    "Nero Sparda",
+    "Lady Nero",
+    "Ryu Hayabusa",
+    "Jin Kazama",
+    "Kazuya Mishima",
+    "Nina Williams",
+    "Heihachi Mishima",
+    "Ken Masters",
+    "Ryu Hoshi",
+    "Chun-Li Zhang",
+    "M. Bison",
+    "Akuma Gouki",
+    "Sagat Muay Thai",
+    "Cammy White",
+    "Blanka Silva",
+    "E. Honda",
+    "Felicia Aensland",
+    "Jill Valentine",
+    "Chris Redfield",
+    "Leon Kennedy",
+    "Claire Redfield",
+    "Dante Sparda",
+    "Luffy Monkey",
+    "Zoro Roronoa",
+    "Nami Swashbuckler",
+    "Usopp Sniper",
+    "Sanji Vinsmoke",
+    "Tony Tony Chopper",
+    "Nico Robin",
+    "Vivi Nefertari",
+    "Shanks Red",
+    "Ace Portgas",
+    "Izuku Midoriya",
+    "Katsuki Bakugo",
+    "Shoto Todoroki",
+    "Eijiro Kirishima",
+    "Tenya Iida",
+    "Tsuyu Asui",
+    "Momo Yaoyorozu",
+    "Kanao Tsuyuri",
+    "Tanjiro Kamado",
+    "Roronoa Zoro",
+    "Mikasa Ackerman",
+    "Armin Arlert",
+    "Jean Kirstein",
+    "Levi Ackerman",
+    "Hange Zoë",
+    "Reiner Braun",
+    "Eren Yeager",
+    "Maki Oze",
+    "Akane Tsunemori",
+    "Kogami Shinya",
+    "Shinya Kogami",
+    "Saitama Amai",
+    "Fubuki Kakashi",
+    "Kenshiro Kasumi",
+    "Jotaro Kujo",
+    "Joseph Joestar",
+    "Giorno Giovanna",
+    "Naruto Uzumaki",
+    "Sasuke Uchiha",
+    "Kakashi Hatake",
+    "Hinata Hyuga",
+    "Sakura Haruno",
+    "Shikamaru Nara",
+    "Iruka Umino",
+    "Neji Hyuga",
+    "Killer Bee",
+    "Itachi Uchiha",
+    "Madara Uchiha",
+    "Minato Namikaze",
   ];
 
-const calculateSkewMultiplier = () => {
-  let sum = 0;
-  
-  for (let i = 5; i <= namesArray.length; i++) {
-    if (i % 2 === 0) {
-      sum -= 0.3;  // Even steps
-    } else {
-      sum -= 0.2;  // Odd steps
-    }
+  const initialArrayLength = namesArray.length;
+
+  if (namesArray.length === 1) {
+    namesArray = [namesArray[0], namesArray[0], namesArray[0], namesArray[0]];
+  } else if (namesArray.length === 2) {
+    namesArray = [namesArray[0], namesArray[0], namesArray[1], namesArray[1]];
+  } else if (namesArray.length === 3) {
+    namesArray = [
+      namesArray[0],
+      namesArray[0],
+      namesArray[1],
+      namesArray[1],
+      namesArray[2],
+      namesArray[2],
+    ];
   }
-  
-  return sum;
-}
 
-let skewMultiplier = calculateSkewMultiplier()
+  const calculateSkewMultiplier = () => {
+    let sum = 0;
 
-const skewArray = [
-  0, 0, 0, 0, 0, 
-  -0.2, -0.5, -0.7, -1, -1.2, -1.5, 
-  -1.7, -2, -2.2, -2.5, -2.7, 
-  -3, -3.2, -3.5, -3.7, -4, 
-  -4.2, -4.5, -4.7, -5, -5.2, 
-  -5.5, -5.7, -6, -6.2, -6.5, 
-  -6.7, -7, -7.2, -7.5, -7.7, 
-  -8, -8.2, -8.5, -8.7, -9, 
-  -9.2, -9.5, -9.7, -10, -10.2, 
-  -10.5, -10.7, -11, -11.2, -11.5, 
-  -11.7, -12, -12.2, -12.5, -12.7, 
-  -13, -13.2, -13.5, -13.7, -14, 
-  -14.2, -14.5, -14.7, -15, -15.2, 
-  -15.5, -15.7, -16, -16.2, -16.5, 
-  -16.7, -17, -17.2, -17.5, -17.7, 
-  -18, -18.2, -18.5, -18.7, -19, 
-  -19.2, -19.5, -19.7, -20, -20.2, 
-  -20.5, -20.7, -21, -21.2, -21.5, 
-  -21.7, -22, -22.2, -22.5, -22.7, 
-  -23, -23.2, -23.5, -23.7, -24, 
-  -24.2, -24.5, -24.7, -25, -25.2, 
-  -25.5, -25.7, -26, -26.2, -26.5, 
-  -26.7, -27, -27.2, -27.5, -27.7, 
-  -28, -28.2, -28.5, -28.7, -29, 
-  -29.2, -29.5, -29.7, -30, -30.2, 
-  -30.5, -30.7, -31, -31.2, -31.5, 
-  -31.7, -32, -32.2, -32.5, -32.7, 
-  -33, -33.2, -33.5, -33.7, -34, 
-  -34.2, -34.5, -34.7, -35, -35.2, 
-  -35.5, -35.7, -36, -36.2, -36.5, 
-  -36.7, -37, -37.2, -37.5, -37.7, 
-  -38, -38.2, -38.5, -38.7, -39, 
-  -39.2, -39.5, -39.7, -40, -40.2, 
-  -40.5, -40.7, -41, -41.2, -41.5, 
-  -41.7, -42, -42.2, -42.5, -42.7, 
-  -43, -43.2, -43.5, -43.7, -44, 
-  -44.2, -44.5, -44.7, -45, -45.2, 
-  -45.5, -45.7, -46, -46.2, -46.5, 
-  -46.7, -47, -47.2, -47.5, -47.7, 
-  -48, -48.2, -48.5, -48.7, -49, 
-  -49.2, -49.5, -49.7, -50, -50.2, 
-  -50.5, -50.7, -51, -51.2, -51.5, 
-  -51.7, -52, -52.2, -52.5, -52.7, 
-  -53, -53.2, -53.5, -53.7, -54, 
-  -54.2, -54.5, -54.7, -55, -55.2
-];
+    for (let i = 5; i <= namesArray.length; i++) {
+      sum -= 0.25;
+    }
+
+    return sum;
+  };
+
+  const skewMultiplier = calculateSkewMultiplier();
+  let swapColors = false;
+
+  const getInitials = (name, index) => {
+    if (namesArray[index] === namesArray[index - 1]) {
+      swapColors = !swapColors;
+      return "";
+    }
+
+    const nameParts = name.split(" ");
+    if (nameParts.length === 1) {
+      return nameParts[0].charAt(0).toUpperCase();
+    } else {
+      const firstInitial = nameParts[0].charAt(0).toUpperCase();
+      const lastInitial = nameParts[nameParts.length - 1]
+        .charAt(0)
+        .toUpperCase();
+      return firstInitial + lastInitial;
+    }
+  };
+  let previousColor;
+
+  const getSegmentColor = (name, index) => {
+    if (namesArray[index] === namesArray[index - 1]) {
+      return previousColor;
+    }
+
+    if (initialArrayLength === 2) {
+      if (index < 2) {
+        previousColor = "rgb(255, 69, 0)";
+        return "rgb(255, 69, 0)";
+      } else {
+        previousColor = "rgb(60, 179, 113)";
+        return "rgb(60, 179, 113)";
+      }
+    } else if (initialArrayLength === 3) {
+      if (index < 2) {
+        previousColor = "rgb(255, 69, 0)";
+        return "rgb(255, 69, 0)";
+      } else if (index < 4) {
+        previousColor = "rgb(60, 179, 113)";
+        return "rgb(60, 179, 113)";
+      } else {
+        previousColor = "rgb(72, 61, 139)";
+        return "rgb(72, 61, 139)";
+      }
+    }
+
+    if (index === 0) {
+      previousColor = "rgb(255, 69, 0)";
+      return "rgb(255, 69, 0)";
+    } else if (
+      (index % 2 === 0 && !swapColors) ||
+      (index % 2 !== 0 && swapColors)
+    ) {
+      previousColor = "rgb(60, 179, 113)";
+      return "rgb(60, 179, 113)";
+    }
+    if ((index % 2 !== 0 && !swapColors) || (index % 2 === 0 && swapColors)) {
+      previousColor = "rgb(72, 61, 139)";
+      return "rgb(72, 61, 139)";
+    }
+  };
 
   const [spinAngle, setSpinAngle] = useState(0);
   const [result, setResult] = useState(null);
@@ -94,6 +218,12 @@ const skewArray = [
 
   return (
     <section className="wheelSection">
+      <div
+        className="result"
+        style={{ visibility: result === null ? "hidden" : "visible" }}
+      >
+        Result: {result}
+      </div>
       <div className="wheelContainer">
         <div className="pointer"></div>
         <div
@@ -103,11 +233,10 @@ const skewArray = [
             transition: "transform 2s ease-out",
           }}
         >
-          {namesArray.map((number, index) => {
+          {namesArray.map((name, index) => {
             const angle = 360 / namesArray.length;
-            const rotation = angle * index;       
+            const rotation = angle * index;
             const skew = angle * skewMultiplier;
-            // const skew = angle * skewArray[namesArray.length];
 
             return (
               <div
@@ -119,11 +248,19 @@ const skewArray = [
                   className="segment"
                   style={{
                     transform: `skewY(${skew}deg)`,
+                    backgroundColor: getSegmentColor(name, index), // Set the background color dynamically
                   }}
                 ></div>
-                <div className="number">
-                  {/* <div className="number" style={{ marginLeft: `${margin}px`, transform: `rotate(25deg)` }}> */}
-                  {number}
+                <div
+                  className="name"
+                  style={{
+                    fontSize: `${Math.max(
+                      4,
+                      Math.min(40, Math.floor(1000 / namesArray.length))
+                    )}px`,
+                  }}
+                >
+                  {getInitials(name, index)}
                 </div>
               </div>
             );
@@ -133,12 +270,6 @@ const skewArray = [
       <button className="spinBtn" onClick={handleSpin}>
         Spin
       </button>
-      <div
-        className="result"
-        style={{ visibility: result === null ? "hidden" : "visible" }}
-      >
-        Result: {result}
-      </div>{" "}
     </section>
   );
 };
